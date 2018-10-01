31fa4d7b66b8ac6ad1ac3fd1b97d0b651a91d92a
Brian Birdwell <bbirdwell@gmail.com>
2018-10-01 17:03:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,13 +31,15 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  
+  shipping_manifest["muskets"] = 2
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest
 
   # return the shipping_manifest hash below
 
